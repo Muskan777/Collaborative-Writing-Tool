18099d84e46fa51baf4e638f7c003eded2b0c0b3
Muskan Paryani <muskanparyani007@gmail.com>
2020-06-30 13:52:15 +0530

    Initial

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const path = require("path");
+var express = require("express");
+var app = express();
+
+const port = process.env.PORT || 3000;
+
+var publicPath = (path.join(__dirname, "../public"));
+
+app.use(express.static(publicPath));
+
+app.listen(port, function() {
+	console.log('Server is up on port ' + port);
+})
